0632b4816a67009fec133b987f3b5466f9b4b976
Mark Baker <mrbaker1917@gmail.com>
2018-07-19 18:42:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var katzDeli = [];
+var katzDeliLine = [];
+
+function takeANumber(katzDeli, name) {
+  katzDeli.push(name);
+  katzDeliLine = katzDeli.length;
+  return ("Welcome, " + name + ". You are number " + katzDeliLine + " in line.");
+}
+
+function nowServing(array) {
+  if (array.length > 0) {
+    var name = array.shift();
+  return ("Currently serving " + name + ".");
+  } else {
+    return ("There is nobody waiting to be served!");
+  }
+}
+
+function currentLine(array) {
+  for (var i = 0; i < array.length; i++) {
+    
+  }
+  return
+}
